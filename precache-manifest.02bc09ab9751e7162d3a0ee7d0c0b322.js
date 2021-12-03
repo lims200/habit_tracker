ecd970aecbf1659adcdc5a9fea308da25c12b363
lims <tkdals875@gmail.com>
2021-12-03 16:36:52 +0900

    Updates

---
+++ precache-manifest.02bc09ab9751e7162d3a0ee7d0c0b322.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d7c71fdace121156de95a9f84669137",
+    "url": "/habit_tracker/index.html"
+  },
+  {
+    "revision": "3e9103f0cb5cda2a1f21",
+    "url": "/habit_tracker/static/css/main.8b73e418.chunk.css"
+  },
+  {
+    "revision": "30ea2229acc1adba22ad",
+    "url": "/habit_tracker/static/js/2.e8da4bea.chunk.js"
+  },
+  {
+    "revision": "1ea337ab0f6eaf802a045a4531cad9ed",
+    "url": "/habit_tracker/static/js/2.e8da4bea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e9103f0cb5cda2a1f21",
+    "url": "/habit_tracker/static/js/main.3efd1267.chunk.js"
+  },
+  {
+    "revision": "dbb297bb1ca874a80b51",
+    "url": "/habit_tracker/static/js/runtime-main.bec78f78.js"
+  }
+]);
